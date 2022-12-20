dd6cb9a7a29226f77b0e5c95e46066eed7219671
Eduardo Pinheiro <72766650+odraudep@users.noreply.github.com>
2022-12-19 23:17:04 -0300

    add base project with tailwindcss

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-slate-900 text-white">
+      <div className="container mx-auto">
+        <h1>Hi</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
