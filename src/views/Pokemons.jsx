23bb7ebd38341c59e75b79a2ba5ce9701badae41
Eduardo Pinheiro <72766650+odraudep@users.noreply.github.com>
2022-12-28 20:08:20 -0300

    add load more pokemons on scroll feature

--- src/views/Pokemons.jsx
+++ src/views/Pokemons.jsx
@@ -1,18 +1,59 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
 import PokemonItem from '@components/PokemonItem';
 
 function Pokemons() {
   const [pokemons, setPokemons] = useState([]);
+  const [nextPokemons, setNextPokemons] = useState();
+  const timeToFetchMorePokemons = useRef(null);
 
   useEffect(() => {
     axios
-      .get('/pokemon?limit=10&offset=0')
-      .then((res) => setPokemons(res.data.results))
+      .get('/pokemon?limit=20&offset=0')
+      .then((res) => {
+        setPokemons(res.data.results);
+
+        setNextPokemons(res.data.next);
+      })
       .catch((err) => {
         throw err;
       });
   }, []);
+
+  const fetchMorePokemons = () => {
+    axios
+      .get(nextPokemons)
+      .then((res) => {
+        setPokemons((prev) => [...prev, ...res.data.results]);
+        return setNextPokemons(res.data.next);
+      })
+      .catch((err) => {
+        throw err;
+      });
+  };
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (timeToFetchMorePokemons.current) {
+        clearTimeout(timeToFetchMorePokemons.current);
+      }
+
+      const isScrollEnd = window.scrollY + window.innerHeight >= document.body.scrollHeight;
+
+      if (isScrollEnd) {
+        timeToFetchMorePokemons.current = setTimeout(fetchMorePokemons, 1000);
+      }
+    };
+
+    if (nextPokemons) {
+      window.addEventListener('scroll', handleScroll);
+    }
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+      clearTimeout(timeToFetchMorePokemons.current);
+    };
+  }, [nextPokemons]);
 
   return (
     <div>
